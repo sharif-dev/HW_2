3d07d68ac45bf6a4d8c69cc42e852064db6af22a
mahsiat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2020-04-29 19:20:47 +0430

    Shake & ui implement

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,48 +9,133 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = getClass().getSimpleName();
+
+    private Switch sw1,sw2, sw3;
+    private EditText et1, et2, et3;
+    private Button getBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        sw1 = (Switch)findViewById(R.id.switch1);
+        sw2 = (Switch)findViewById(R.id.switch2);
+        sw3 = (Switch)findViewById(R.id.switch3);
+        et1 = (EditText)findViewById(R.id.et1);
+        et2 = (EditText)findViewById(R.id.et2);
+        et3 = (EditText)findViewById(R.id.et3);
+        getBtn = (Button)findViewById(R.id.getBtn);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+//                alarm service should run
+                    Log.d(TAG, "Alarm on");
+                    String timeToAlarm = et1.getText().toString();
+//                    todo sabrine:
+//                    1. change string to time value
+//                    2. if any error : make switch off & toast error
+//                    3. else : turn on the alarm
+
+                }
+                else {
+                    Log.d(TAG, "Alarm off");
+//                    todo Sabrine:
+//                    turn off the alarm
+                }
             }
         });
+
+
+
+        sw2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+//                    shake service should run
+                    Log.d(TAG, "Shake on");
+
+
+                    String sensibilityToShake = et2.getText().toString();
+                    try {
+                        float number = Float.parseFloat(sensibilityToShake);
+                        MotionDetector.setShakeThreshold(number);
+                        Intent accelerationIntent = new Intent(MainActivity.this, MotionDetector.class);
+                        startService(accelerationIntent);
+
+                    } catch (NumberFormatException e) {
+                       Log.d(TAG, "Not a float input");
+                       sw2.setChecked(false);
+                    }
+
+                }
+                else {
+                    Log.d(TAG, "Shake off");
+                    Intent accelerationIntent = new Intent(MainActivity.this, MotionDetector.class);
+                    stopService(accelerationIntent);
+
+                }
+            }
+        });
+
+        sw3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+//                    shake service should run
+                    Log.d(TAG, "Sleep on");
+
+
+                    String degreeToSleep = et3.getText().toString();
+                    try {
+                        float number = Float.parseFloat(degreeToSleep);
+                        MotionDetector.setShakeThreshold(number);
+//                      todo Ali:
+//                        run service
+
+                    } catch (NumberFormatException e) {
+                        Log.d(TAG, "Not a float input");
+                        sw2.setChecked(false);
+                    }
+
+                }
+                else {
+                    Log.d(TAG, "Sleep off");
+//                    todo Ali:
+//                    stop service
+
+
+                }
+            }
+        });
+
+
+
+
+
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/example/myapplication/MotionDetector.java
@@ -0,0 +1,112 @@
+package com.example.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MotionDetector extends Service implements SensorEventListener {
+
+    final String TAG = getClass().getSimpleName();
+
+    float xAccel, yAccel, zAccel;
+    float xPreviousAccel, yPreviousAccel, zPreviousAccel;
+    boolean firstUpdate = true;
+    boolean shakeInitiated = false;
+    Sensor accelerometer;
+    SensorManager sm;
+    private static float shakeThreshold = 4.5f;
+
+    static public void setShakeThreshold(float shakeThreshold_) {
+        shakeThreshold = shakeThreshold_;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "Service Started");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "on sensor changed called");
+
+
+        updateAcceleratorParameters(event.values[0], event.values[1], event.values[2]);
+        boolean isAccelerationDetected = isAccelerationDetected();
+        Log.d(TAG, "is accelaration detected: " + isAccelerationDetected);
+
+        if ((!shakeInitiated) && (isAccelerationDetected)) {
+            shakeInitiated = true;
+        } else if ((shakeInitiated) && (!isAccelerationDetected)) {
+            shakeInitiated = false;
+        } else if ((shakeInitiated) && (isAccelerationDetected)) {
+            executeShakeAction();
+        }
+
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void executeShakeAction() {
+        Toast.makeText(getApplicationContext(), "Shake detected", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Shake detected");
+
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Shake destroyed");
+        sm.unregisterListener(this);
+
+    }
+
+    private boolean isAccelerationDetected() {
+        int temp = 0;
+        if (Math.abs(xPreviousAccel - xAccel) > shakeThreshold) temp += 1;
+        if (Math.abs(yPreviousAccel - yAccel) > shakeThreshold) temp += 1;
+        if (Math.abs(zPreviousAccel - zAccel) > shakeThreshold) temp += 1;
+        Log.d(TAG, "temp value: " + temp);
+        return temp > 1;
+    }
+
+    private void updateAcceleratorParameters(float xNewAccel, float yNewAccel, float zNewAccel) {
+        if (firstUpdate) {
+            xPreviousAccel = xNewAccel;
+            yPreviousAccel = yNewAccel;
+            zPreviousAccel = zNewAccel;
+            firstUpdate = false;
+        } else {
+            xPreviousAccel = xAccel;
+            yPreviousAccel = yAccel;
+            zPreviousAccel = zAccel;
+        }
+
+        xAccel = xNewAccel;
+        yAccel = yNewAccel;
+        zAccel = zNewAccel;
+    }
+}
