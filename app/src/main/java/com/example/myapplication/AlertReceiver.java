9f16fe891ce998e731f38be77adebcfe501dff90
SabrinehM <Sabrinehmokhtari@yahoo.com>
2020-04-30 00:21:42 +0430

    Receiver Added for handling the alarm

--- app/src/main/java/com/example/myapplication/AlertReceiver.java
+++ app/src/main/java/com/example/myapplication/AlertReceiver.java
@@ -3,11 +3,21 @@ package com.example.myapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.widget.Toast;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 public class AlertReceiver extends BroadcastReceiver {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent intent1 = new Intent(context, AlarmActivity.class);
-        context.startActivity(intent1);
+        NotifHandler notifHandler = new NotifHandler(context);
+        NotificationCompat.Builder nb = notifHandler.getChannelNotification("Alarm", "Wake Up");
+        notifHandler.getManager().notify(1, nb.build());
+
+        Intent alarmActivity = new Intent(context, AlarmActivity.class);
+        context.startActivity(alarmActivity);
     }
 }
