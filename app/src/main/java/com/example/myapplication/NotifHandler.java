68790d49cebc08a3c3a800b50fad33d4c08589f5
SabrinehM <Sabrinehmokhtari@yahoo.com>
2020-04-30 00:22:04 +0430

    Notification Created for the alarm

---
+++ app/src/main/java/com/example/myapplication/NotifHandler.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+public class NotifHandler extends ContextWrapper {
+    private static final String ChannelID = "ChannelID";
+    private static final String ChannelName = "Channel";
+    private NotificationManager mManager;
+
+    public NotifHandler(Context base) {
+        super(base);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ConfigChannels();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void ConfigChannels() {
+        NotificationChannel channel1 = new NotificationChannel(ChannelID, ChannelName, NotificationManager.IMPORTANCE_DEFAULT);
+        channel1.enableLights(true);
+        channel1.enableVibration(true);
+        channel1.setLightColor(R.color.colorPrimary);
+        channel1.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel1);
+    }
+
+    public NotificationManager getManager(){
+        if (mManager == null){
+            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return mManager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification(String title, String message){
+        return new NotificationCompat.Builder(getApplicationContext(), ChannelID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setVibrate(new long[] { 0, 1000, 1000, 1000, 1000})
+                .setSmallIcon(R.drawable.ic_alarm_green);
+
+    }
+}
